01fc9d747f155f6edd0a2866d147f347ac6b4e1e
kanishqtalentconnect-prog <kanishq.talentconnect@gmail.com>
2026-02-04 14:47:54 +0530

    Resolved Vercel

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 export function useScrollSpy(sectionIds: string[]) {
